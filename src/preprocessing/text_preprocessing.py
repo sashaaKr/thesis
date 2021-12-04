1ccdbdc09535237c52ce356ae16ef9dd53c55783
sasha <zooblin@gmail.com>
2021-12-04 13:15:31 +0200

    implementing similarities with gensim

---
+++ src/preprocessing/text_preprocessing.py
@@ -0,0 +1,11 @@
+from gensim import corpora
+
+def create_bag_of_words(corpus):
+    processed_corpus = [ [ word for word in document.split() ] for document in corpus] 
+    dictionary = create_dictionary(corpus)
+    return [dictionary.doc2bow(text) for text in processed_corpus]
+    # return [ [ word for word in document.split() ] for document in corpus] 
+
+def create_dictionary(corpus):
+    processed_corpus = [ [ word for word in document.split() ] for document in corpus] 
+    return corpora.Dictionary(processed_corpus)
