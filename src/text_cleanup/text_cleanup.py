2d85a15b5756d60a8b373cd029e6b32f374795d2
sasha <zooblin@gmail.com>
2022-03-31 07:02:48 +0300

    replacing by yoni doc

--- src/text_cleanup/text_cleanup.py
+++ src/text_cleanup/text_cleanup.py
@@ -12,6 +12,9 @@ def cleanup(t):
     # this regex handle (1r) and (49r) references that appear in b_london version
     # and (112vb) and (117r) that appear in a_zwickau version
     clean_data = re.sub(r'\([1-9][0-9]?[0-9]?[a-z][a-z]?\)', '', t)
+
+    # this regex handle 33o, 3o, 1283o in zwickau version
+    clean_data = re.sub(r'[1-9][0-9]?[0-9]?[0-9]?o', '', clean_data)
     
     # remove numbers
     clean_data = re.sub(r'[0-9]+', '', clean_data)
@@ -41,10 +44,12 @@ def cleanup(t):
         .replace("”", "")
         .replace("(", "")
         .replace(")", "")
+        .replace('y' , 'i') # from Yoni table
         .replace("ff", "f") # from Yoni table
         .replace("ll", "l") # from Yoni table
         .replace("mm", "m") # from Yoni table
-        .replace("tt", "t") # from Yoni table
+        .replace('th', 't') # from Yoni table
+        .replace("tt", "t") # from Yoni table, MUST be be after th -> t
         .replace("z", "s") # from Yoni table
         .replace("ih", "i") # from Yoni table
         .replace("Ih", "i") # from Yoni table
@@ -54,8 +59,11 @@ def cleanup(t):
         # .replace("ch", "c") # from Yoni table
         .replace("cio", "tio") # from Yoni table
         .replace("cia", "tia") # from Yoni table
+        .replace("ch", "c") # from Yoni table, MUST be after cia -> tia
         .replace("tiu", "ciu") # from Yoni table
         .replace("atque", "et") # from Yoni table
+        .replace("uel", "aut") # from Yoni table vel-->aut, cause of jv replacer it is changed
+        
         .replace("  ", " ")
         .strip()
     )
@@ -65,11 +73,16 @@ def cleanup(t):
 def create_corpus_by_line(raw_text):
     lowered_text = raw_text.lower()
     tokenizer = LineTokenizer('latin')
+
+    # without that rule: Mons Bethulie Inde ad 3 leucis est mons Bethulie, ubi Iudit occidit Holofernem.  Qui mons per totam fere Galileam videtur, pulcher valde et firmus.
+    # will be distinguished as 2 lines
+    lowered_text = lowered_text.replace(" ", "")
     
     return [ cleanup(t) for t in tokenizer.tokenize(lowered_text) ]
 
 def create_corpus_by_3_sentences(raw_text):
     lowered_text = raw_text.lower()
+    lowered_text = lowered_text.replace(" ", "")
     tokenizer = LineTokenizer('latin')
     corpus_by_line = tokenizer.tokenize(lowered_text)
 
--- src/text_cleanup/text_cleanup.test.py
+++ src/text_cleanup/text_cleanup.test.py
@@ -2,83 +2,105 @@ import unittest
 import text_cleanup.text_cleanup as text_cleanup
 
 original_vs_expexted = [
-    # [
-    #     "(112vb) Incipit descripcio terre sancte",
-    #     "Incipit descriptio terre sancte"
-    # ],
-    # [
-    #     "Bericus Ultra Sydonem ad 4 leucas est (117r) nobilis illa et antiqua civitas Beritus, ubi dicitur etiam dominus predicasse, ubi Iudei, facta ymagine salvatoris de pasta, ei illuserunt et tandem crucifigentes ab ea sanguine in magna quantitate extraxerunt, qui usque hodie in multis locis venerationi habetur",
-    #     "Bericus Ultra Sydonem ad leucas est nobilis ila et antiqua civitas Beritus ubi dicitur etiam dominus predicasse ubi Iudei facta ymagine salvatoris de pasta ei iluserunt et tandem crucifigentes ab ea sanguine in magna quantitate extraxerunt qui usque hodie in multis locis venerationi habetur"
-    # ],
-    # [
-    #     "In(123r)de ad ½ leucam est Dothaim, de qua dictum est supra.",
-    #     "Inde ad leucam est Dothaim de qua dictum est supra"
-    # ],
-    # [
-    #     "  Qui mons per totam fere Galileam videtur, pulcher valde et firmus.",
-    #     "Qui mons per totam fere Galileam videtur pulcher valde et firmus"
-    # ],
-    # [
-    #     "Iesrahel pulchrum habet prospectum per totam Galileam usque ad Carmelum et montes Phenicie montemque Thabor montemque Galaat ultra Iordanem et totum montem Effraym iterum usque ad Carmelum.",
-    #     "Iesrahel pulchrum habet prospectum per totam Galileam usque ad Carmelum et montes penicie montemque Thabor montemque Galaat ultra Iordanem et totum montem Efraym iterum usque ad Carmelum"
-    # ],
-    # [
-    #     "De Balonia recedens ductus sum ad locum ubi erant 6 leones et 6 elephantes et 60 strutones in curia et onagri plures.",
-    #     "De Balonia recedens ductus sum ad locum ubi erant leones et elepantes et strutones in curia et onagri plures"
-    # ],
-    # [
-    #     # removes double ll
-    #     'Ego autem credo illud pocius esse Chanaam filii Cham filii Noe vel alicuius filiorum eius, qui ad terram circa loca illa habitasse.',
-    #     'Ego autem credo ilud pocius esse Chanaam filii Cham filii Noe vel alicuius filiorum eius qui ad terram circa loca ila habitasse'
-    # ],
-    # [
-    #     # removes double mm
-    #     'Fures qui minora furta committunt, ut non sint digni suspendio, vel malefactores alii commitentes crimina minora, castrantur, ne filios generent, qui patrum crimina imitentur et hec videtur mihi una causa, quare tot meretrices sunt in partibus illis.',
-    #     'Fures qui minora furta comitunt ut non sint digni suspendio vel malefactores alii comitentes crimina minora castrantur ne filios generent qui patrum crimina imitentur et hec videtur mii una causa quare tot meretrices sunt in partibus ilis'
-    # ],
-    # [
-    #     # removes double tt
-    #     'Via occidentis Hec autem linea ad litteram est via illa, de qua dicitur Thobias 1.',
-    #     'Via occidentis Hec autem linea ad literam est via ila de qua dicitur Thobias'
-    # ],
-    # [
-    #     #change z->s
-    #     'Accedit insuper ad easdem piscinas fons Gyon inferior, situs in pede montis Gyon contra agrum Acheldemach iuxta locum, ubi stetit Rapsaces, quando domino exprobavit et Ezechieli et sedenti populo super murum.',
-    #     'Accedit insuper ad easdem piscinas fons Gyon inferior situs in pede montis Gyon contra agrum Acheldemach iuxta locum ubi stetit Rapsaces quando domino exprobavit et Esechieli et sedenti populo super murum'
-    # ],
-    # [
-    #     # change ih -> i
-    #     'Sarraceni Machmetum predicant et legem eius servant, dominum Ihesum Christum maximum prophetarum dicunt et eundem de spiritu sancto conceptum et de virgine natum, sed neFehler!',
-    #     'Sarraceni Machmetum predicant et legem eius servant dominum iesum Christum maximum propetarum dicunt et eundem de spiritu sancto conceptum et de virgine natum sed neFehler'
-    # ],
-    # [
-    #     'Secunda Palestina est cuius metropolis est Cesarea Palestine sive Cesarea Martania cum tota terra <palasti> Philistim, incipiens a predicta Petra Incisa sive Castro Peregrinorum et extendens se usque Gazam et Gerazam.',
-    #     'Secunda Palestina est cuius metropolis est Cesarea Palestine sive Cesarea Martania cum tota terra palasti pilistim incipiens a predicta Petra Incisa sive Castro Peregrinorum et extendens se usque Gasam et Gerasam'
-    # ],
-    # [
-    #     # change ae -> e
-    #     'Inde 4 leucis est civitas antiqua Asor dicta in qua habitavit rex Iabin ille potens, qui cum 24 regibus pugnavit contra Iosue et Israel ad Aquas Maron.',
-    #     'Inde leucis est civitas antiqua Asor dicta in qua habitavit rex Iabin ile potens qui cum regibus pugnavit contra Iosue et Isrel ad Aquas Maron'
-    # ],
-    # [
-    #     # change ch -> c
-    #     'Inde per Anthiochiam veni ad montana magna que sunt promuctorium Sicilie quod dicitur Ratzgenerz i[d est] caput porci, et sic applicuimus in portu Palis et venimus ad Ayaz.',
-    #     'Inde per Anthiociam veni ad montana magna que sunt promuctorium Sicilie quod dicitur Ratsgeners id est caput porci et sic applicuimus in portu Palis et venimus ad Ayas'
-    # ]
+    [
+        "(112vb) Incipit descripcio terre sancte", # original text
+        "incipit descriptio terre sancte" # expected text after cleanup
+    ],
+    [
+        "Bericus Ultra Sydonem ad 4 leucas est (117r) nobilis illa et antiqua civitas Beritus, ubi dicitur etiam dominus predicasse, ubi Iudei, facta ymagine salvatoris de pasta, ei illuserunt et tandem crucifigentes ab ea sanguine in magna quantitate extraxerunt, qui usque hodie in multis locis venerationi habetur",
+        "bericus ultra sidonem ad leucas est nobilis ila et antiqua ciuitas beritus ubi dicitur etiam dominus predicasse ubi iudei facta imagine saluatoris de pasta ei iluserunt et tandem crucifigentes ab ea sanguine in magna quantitate extraxerunt qui usque hodie in multis locis uenerationi habetur"
+    ],
+    [
+        "In(123r)de ad ½ leucam est Dothaim, de qua dictum est supra.",
+        "inde ad leucam est dotaim de qua dictum est supra"
+    ],
+    [
+        # "  Qui mons per totam fere Galileam videtur, pulcher valde et firmus.",
+        'Mons Bethulie Inde ad 3 leucis est mons Bethulie, ubi Iudit occidit Holofernem.  Qui mons per totam fere Galileam videtur, pulcher valde et firmus.',
+        "mons betulie inde ad leucis est mons betulie ubi iudit occidit holofernem qui mons per totam fere galileam uidetur pulcer ualde et firmus"
+    ],
+    [
+        "Iesrahel pulchrum habet prospectum per totam Galileam usque ad Carmelum et montes Phenicie montemque Thabor montemque Galaat ultra Iordanem et totum montem Effraym iterum usque ad Carmelum.",
+        "iesrahel pulcrum habet prospectum per totam galileam usque ad carmelum et montes penicie montemque tabor montemque galaat ultra iordanem et totum montem efraim iterum usque ad carmelum"
+    ],
+    [
+        "De Balonia recedens ductus sum ad locum ubi erant 6 leones et 6 elephantes et 60 strutones in curia et onagri plures.",
+        "de balonia recedens ductus sum ad locum ubi erant leones et elepantes et strutones in curia et onagri plures"
+    ],
+    [
+        # removes double ll
+        'Ego autem credo illud pocius esse Chanaam filii Cham filii Noe vel alicuius filiorum eius, qui ad terram circa loca illa habitasse.',
+        'ego autem credo ilud pocius esse canaam filii cam filii noe aut alicuius filiorum eius qui ad terram circa loca ila habitasse'
+    ],
+    [
+        # removes double mm
+        'Fures qui minora furta committunt, ut non sint digni suspendio, vel malefactores alii commitentes crimina minora, castrantur, ne filios generent, qui patrum crimina imitentur et hec videtur mihi una causa, quare tot meretrices sunt in partibus illis.',
+        'fures qui minora furta comitunt ut non sint digni suspendio aut malefactores alii comitentes crimina minora castrantur ne filios generent qui patrum crimina imitentur et hec uidetur mii una causa quare tot meretrices sunt in partibus ilis'
+    ],
+    [
+        # removes double tt
+        'Via occidentis Hec autem linea ad litteram est via illa, de qua dicitur Thobias 1.',
+        'uia occidentis hec autem linea ad literam est uia ila de qua dicitur tobias'
+    ],
+    [
+        #change z->s
+        'Accedit insuper ad easdem piscinas fons Gyon inferior, situs in pede montis Gyon contra agrum Acheldemach iuxta locum, ubi stetit Rapsaces, quando domino exprobavit et Ezechieli et sedenti populo super murum.',
+        'accedit insuper ad easdem piscinas fons gion inferior situs in pede montis gion contra agrum aceldemac iuxta locum ubi stetit rapsaces quando domino exprobauit et esecieli et sedenti populo super murum'
+    ],
+    [
+        # change ih -> i
+        'Sarraceni Machmetum predicant et legem eius servant, dominum Ihesum Christum maximum prophetarum dicunt et eundem de spiritu sancto conceptum et de virgine natum, sed neFehler!',
+        'sarraceni macmetum predicant et legem eius seruant dominum iesum cristum maximum propetarum dicunt et eundem de spiritu sancto conceptum et de uirgine natum sed nefehler'
+    ],
+    [
+        'Secunda Palestina est cuius metropolis est Cesarea Palestine sive Cesarea Martania cum tota terra <palasti> Philistim, incipiens a predicta Petra Incisa sive Castro Peregrinorum et extendens se usque Gazam et Gerazam.',
+        'secunda palestina est cuius metropolis est cesarea palestine siue cesarea martania cum tota terra palasti pilistim incipiens a predicta petra incisa siue castro peregrinorum et extendens se usque gasam et gerasam'
+    ],
+    [
+        # change ae -> e
+        'Inde 4 leucis est civitas antiqua Asor dicta in qua habitavit rex Iabin ille potens, qui cum 24 regibus pugnavit contra Iosue et Israel ad Aquas Maron.',
+        'inde leucis est ciuitas antiqua asor dicta in qua habitauit rex iabin ile potens qui cum regibus pugnauit contra iosue et isrel ad aquas maron'
+    ],
+    [
+        # change ch -> c
+        'Inde per Anthiochiam veni ad montana magna que sunt promuctorium Sicilie quod dicitur Ratzgenerz i[d est] caput porci, et sic applicuimus in portu Palis et venimus ad Ayaz.',
+        'inde per antiociam ueni ad montana magna que sunt promuctorium sicilie quod dicitur ratsgeners id est caput porci et sic applicuimus in portu palis et uenimus ad aias'
+    ],
     [
         # atque -> et
         'Alius mons Seyr coniungitur cum deserto Pharan, quem longo tempore circuierunt et de illo dicitur Deuteronomius 3o: „In Monte Seyr olim habitaverunt horrei, quibus eiectis atque deletis habitaverunt filii Esau pro eis“.',
-        'Alius mons Seyr coniungitur cum deserto paran quem longo tempore circuierunt et de ilo dicitur Deuteronomius o In Monte Seyr olim habitaverunt horrei quibus eiectis et deletis habitaverunt filii Esau pro eis'
+        'alius mons seir coniungitur cum deserto paran quem longo tempore circuierunt et de ilo dicitur deuteronomius in monte seir olim habitauerunt horrei quibus eiectis et deletis habitauerunt filii esau pro eis'
+    ],
+    [
+        # change vel -> aut
+        'Quem enim hora diei vel noctis per totum anni circulum in qua non recolit cantando legendo psallendo predicando et meditando omnis devotus Christianus que facta sunt vel scripta in hac terra et civitatibus et locis eius.',
+        'quem enim hora diei aut noctis per totum anni circulum in qua non recolit cantando legendo psalendo predicando et meditando omnis deuotus cristianus que facta sunt aut scripta in hac terra et ciuitatibus et locis eius'
+    ],
+    [
+        # remove number with o postfix
+        "Alia insuper vice scilicet anno domini 1283o in festo Omnium Sanctorum sub divo dormiens in eodem monte cum aliis multis fui cum ipsis rore penitus infusus nocte illa.",
+        "alia insuper uice scilicet anno domini in festo omnium sanctorum sub diuo dormiens in eodem monte cum aliis multis fui cum ipsis rore penitus infusus nocte ila"
+    ],
+    [
+        # change th -> t
+        'Thiberias De Bethulia ad 5 leucas et plus supra mare Galilee est Tyberias civitas, a qua idem mare Thyberiadis dicitur, olim Cenereth dicta, a qua etiam mare Cenereth dictum.',
+        'tiberias de betulia ad leucas et plus supra mare galilee est tiberias ciuitas a qua idem mare tiberiadis dicitur olim ceneret dicta a qua etiam mare ceneret dictum'
+    ],
+    [
+        # change y -> i
+        "Fuit autem in campo per longum disposita tendens ab austro in aquilonem sub monte Antylibano inter ipsum et mare spaciosa valde.",
+        "fuit autem in campo per longum disposita tendens ab austro in aquilonem sub monte antilibano inter ipsum et mare spatiosa ualde"
     ]
 ]
 
 class TestTextCleanupMethods(unittest.TestCase):
 
     def test_cleanup(self):
+        self.maxDiff = None
         for original, expected in original_vs_expexted:
-            result = text_cleanup.cleanup(original)
+            result = text_cleanup.create_corpus_by_line(text_cleanup.jvtext(original))
             # print(result)
-            self.assertEqual(result, expected)
+            self.assertEqual(result, [expected])
 
 if __name__ == '__main__':
     unittest.main()
