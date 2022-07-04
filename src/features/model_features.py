2abd678aa43e606a6eecb634c7a2e259e300fe27
sasha <zooblin@gmail.com>
2022-07-04 21:05:12 +0300

    preparation for 10.3

--- src/features/model_features.py
+++ src/features/model_features.py
@@ -135,48 +135,59 @@ def run_models(features_df):
   
   return scores_df
 
-def run_grid_search_cv(features_df):
+def run_grid_search_cv(features_df, classifiers_to_test):
   X, y = create_X_y(features_df)
   X_train, X_test, y_train, y_test = train_test_split(X, y,random_state=101,stratify=y)
 
-  classifiers = [
-    # ( SVC(), 'SVC', { 'C': np.logspace(-3, 2, 6), 'gamma': np.logspace(-3, 2, 6) / X_train.shape[0], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid'] }),
-    # ( DecisionTreeClassifier(), 'DecisionTreeClassifier', { 'criterion': ['gini', 'entropy'], 'max_depth': [None,4,5,6,7,8,9,10,11,12,15,20,30,40,50,70,90,120,150]} ),
-    # ( GaussianProcessClassifier(), 'GaussianProcessClassifier', { 'kernel': [1*RBF(), 1*DotProduct(), 1*Matern(),  1*RationalQuadratic(), 1*WhiteKernel()] } ),
-    # ( RandomForestClassifier(), 'RandomForestClassifier', { 
-    #   'n_estimators': [50, 100, 150, 200, 250, 300, 400, 500], 
-    #   'criterion': ['gini', 'entropy', 'log_loss'], 
-    #   'max_features': ['auto', 'sqrt', 'log2'], 
-    #   'max_depth' : [4,5,6,7,8,9,10,11,12], 
-    #   'random_state': [0], 
-    #   'max_features': ['auto', 'sqrt', 'log2'] 
-    #   } ),
-    # TODO: MLPClassifier didn't work
-    ( MLPClassifier(), 'MLPClassifier', { 
-      'solver': ['lbfgs', 'sgd', 'adam'], 
-      'alpha': 10.0 ** -np.arange(1, 10), 
-      'max_iter': [1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,10000],
-      'learning_rate': ['constant', 'invscaling', 'adaptive'],
-      'activation': ['identity', 'logistic', 'tanh', 'relu'],
-      'hidden_layer_sizes': [(10,), (20,), (40,), (60,), (80,), (100,), (150,), (200,), (300,), (500,), (700,), (900,), (1500,), (2000,), (50,50,50), (50,100,50)] 
-      } ), 
-    # ( GaussianNB(), 'GaussianNB', { 'var_smoothing': np.logspace(0,-9, num=100) } ),
-    # ( KNeighborsClassifier(), 'KNeighborsClassifier', {
-    #   'n_neighbors': [3,5,11,19],
-    #   'weights': ['uniform', 'distance'],
-    #   'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
-    # } ),
-    # ( AdaBoostClassifier(), 'AdaBoostClassifier', { 'n_estimators':[5, 10, 30, 50, 100, 300, 500,1000,2000], 'learning_rate':[.001,0.01,.1, .5, 1, 2] } ),
-    # ( QuadraticDiscriminantAnalysis(), 'QuadraticDiscriminantAnalysis', { 
-    #   'reg_param': [0.00001, 0.0001, 0.001,0.01, 0.1, 0.2, 0.3, 0.4, 0.5], 
-    #   'store_covariance': [True, False],
-    #   'tol': (0.0001, 0.001,0.01, 0.1)
-    #   })
-  ]
+  # classifiers = [
+    
+  #   # TODO: MLPClassifier didn't work
+  #   ( MLPClassifier(), 'MLPClassifier', { 
+  #     'solver': ['lbfgs', 'sgd', 'adam'], 
+  #     'alpha': 10.0 ** -np.arange(1, 10), 
+  #     'max_iter': [1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,10000],
+  #     'learning_rate': ['constant', 'invscaling', 'adaptive'],
+  #     'activation': ['identity', 'logistic', 'tanh', 'relu'],
+  #     'hidden_layer_sizes': [(10,), (20,), (40,), (60,), (80,), (100,), (150,), (200,), (300,), (500,), (700,), (900,), (1500,), (2000,), (50,50,50), (50,100,50)] 
+  #     } ), 
+  # ]
+
+  
+
+  classifiers = {
+    'SVC': ( SVC(), { 'C': np.logspace(-3, 2, 6), 'gamma': np.logspace(-3, 2, 6) / X_train.shape[0], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid'] } ),
+    'DecisionTreeClassifier': (DecisionTreeClassifier(), { 'criterion': ['gini', 'entropy'], 'max_depth': [None,4,5,6,7,8,9,10,11,12,15,20,30,40,50,70,90,120,150]} ),
+    'GaussianProcessClassifier': ( GaussianProcessClassifier(),  { 'kernel': [1*RBF(), 1*DotProduct(), 1*Matern(),  1*RationalQuadratic(), 1*WhiteKernel()] } ),
+    'RandomForestClassifier': ( RandomForestClassifier(),  { 
+      'n_estimators': [50, 100, 150, 200, 250, 300, 400, 500], 
+      'criterion': ['gini', 'entropy', 'log_loss'], 
+      'max_features': ['auto', 'sqrt', 'log2'], 
+      'max_depth' : [4,5,6,7,8,9,10,11,12], 
+      'random_state': [0], 
+      'max_features': ['auto', 'sqrt', 'log2']
+      }),
+  'GaussianNB': ( GaussianNB(), { 'var_smoothing': np.logspace(0,-9, num=100) } ),
+  'KNeighborsClassifier': ( KNeighborsClassifier(),  {
+      'n_neighbors': [3,5,11,19],
+      'weights': ['uniform', 'distance'],
+      'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+      }),
+  'AdaBoostClassifier': ( AdaBoostClassifier(), { 'n_estimators':[5, 10, 30, 50, 100, 300, 500,1000,2000], 'learning_rate':[.001,0.01,.1, .5, 1, 2] } ),
+  'QuadraticDiscriminantAnalysis': ( QuadraticDiscriminantAnalysis(), { 
+      'reg_param': [0.00001, 0.0001, 0.001,0.01, 0.1, 0.2, 0.3, 0.4, 0.5], 
+      'store_covariance': [True, False],
+      'tol': (0.0001, 0.001,0.01, 0.1)
+      })
+  }
+
+  if classifiers_to_test is None: classifiers_to_test = list(classifiers.keys())
+  print(f'testing classifiers: {classifiers_to_test}')
 
   scores_df = pd.DataFrame(dtype=float)
   grid_results = []
-  for classifier, classifier_name, classifiers_grid_search_params in classifiers:
+
+  for classifier_name in classifiers_to_test:
+    classifier, classifiers_grid_search_params = classifiers[classifier_name]
     print(f'running: {classifier_name}')
     param_grid = GridSearchCV(
       classifier, 
@@ -187,6 +198,18 @@ def run_grid_search_cv(features_df):
     )
     param_grid.fit(X_train, y_train)
     grid_results.append(param_grid)
+
+  # for classifier, classifier_name, classifiers_grid_search_params in classifiers:
+    # print(f'running: {classifier_name}')
+    # param_grid = GridSearchCV(
+    #   classifier, 
+    #   classifiers_grid_search_params, 
+    #   return_train_score = True, 
+    #   cv = 10, 
+    #   n_jobs = -1,
+    # )
+    # param_grid.fit(X_train, y_train)
+    # grid_results.append(param_grid)
 
     # scores_df.loc[classifier_name, 'best_score_'] = param_grid.best_score_
     # scores_df.loc[classifier_name, 'best_estimator_'] = param_grid.best_estimator_
