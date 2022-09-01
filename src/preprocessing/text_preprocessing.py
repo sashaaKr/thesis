089f08826575fac6609f22fb9071ca568ad14209
sasha <zooblin@gmail.com>
2022-09-01 20:18:26 +0300

    preparation for 28.8 - clean algorithm and output

--- src/preprocessing/text_preprocessing.py
+++ src/preprocessing/text_preprocessing.py
@@ -1,11 +1,10 @@
 from gensim import corpora
 
 def create_bag_of_words(corpus):
-    processed_corpus = [ [ word for word in document.split() ] for document in corpus] 
-    dictionary = create_dictionary(corpus)
-    return [dictionary.doc2bow(text) for text in processed_corpus]
-    # return [ [ word for word in document.split() ] for document in corpus] 
+  processed_corpus = [ [ word for word in document.split() ] for document in corpus] 
+  dictionary = create_dictionary(corpus)
+  return [dictionary.doc2bow(text) for text in processed_corpus]
 
 def create_dictionary(corpus):
-    processed_corpus = [ [ word for word in document.split() ] for document in corpus] 
-    return corpora.Dictionary(processed_corpus)
+  processed_corpus = [ [ word for word in document.split() ] for document in corpus] 
+  return corpora.Dictionary(processed_corpus)
